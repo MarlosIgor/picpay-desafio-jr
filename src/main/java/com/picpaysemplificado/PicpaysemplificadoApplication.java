88f73d0ac3ce305eff988ae19ac209bf4a665b40
MarlosIgor <marooosigor@gmail.com>
2024-04-15 15:05:10 -0300

    feat: first commit

---
+++ src/main/java/com/picpaysemplificado/PicpaysemplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysemplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysemplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysemplificadoApplication.class, args);
+	}
+
+}
