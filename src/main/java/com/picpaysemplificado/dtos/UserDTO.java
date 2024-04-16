996f02a4316e158b4bf57f49e912cf3963c95e79
MarlosIgor <marooosigor@gmail.com>
2024-04-15 22:12:08 -0300

    feat: create UserDTO

---
+++ src/main/java/com/picpaysemplificado/dtos/UserDTO.java
@@ -0,0 +1,8 @@
+package com.picpaysemplificado.dtos;
+
+import com.picpaysemplificado.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
